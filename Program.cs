6dc1fca67ca896ec232e3dc67ab6f5b59fde117d
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 23:45:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Iveskite atsikelimo laika");
+int time = Convert.ToInt32(Console.ReadLine());
+if (time < 12)
+{
+    Console.WriteLine("Geros dienos!");
+}
+else if (time >= 12 && time <= 18)
+{
+    Console.WriteLine("Geros popietes!");
+}
+else if (time >= 18 && time <= 24)
+{
+    Console.WriteLine("Gero vakaro!");
+}
